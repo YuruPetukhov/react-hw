eade36b7fa4b833b00df4f4765ef48dd5723f069
CyernyavkoJack666 <90631945+CyernyavkoJack666@users.noreply.github.com>
2022-02-06 13:07:05 +0300

    Homework-route-done

---
+++ Homework_lesson_5/src/Aboutpage.tsx
@@ -0,0 +1,12 @@
+
+const Aboutpage = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>This is a demo website</p>
+    </div>
+  )
+  
+};
+
+export  {Aboutpage} 
---
+++ Homework_lesson_5/src/Blogpage.tsx
@@ -0,0 +1,14 @@
+
+const Blogpage = () => {
+  return (
+    <div>
+      <h1>Blog page</h1>
+      <p>This is a blog</p>
+    </div>
+  )
+  
+};
+
+export {Blogpage} 
+
+
